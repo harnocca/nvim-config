97fca7f1ed64c14bf20a6e31c10bd3737d7da3b9
Harnocca <hauseowo@outlook.com>
2026-08-09 13:42:26 +0800

    ui: add markdown support

---
+++ lua/plugins/ui/markdown.lua
@@ -0,0 +1,8 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ft = { "markdown", "quarto" },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-mini/mini.nvim",
+  },
+}
