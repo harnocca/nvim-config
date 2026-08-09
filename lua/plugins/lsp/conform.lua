d8d347730d46f26b080b3b195ea34ac6094e9505
Harnocca <hauseowo@outlook.com>
2026-08-10 02:49:05 +0800

    lsp: disable format-on-save

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -31,9 +31,9 @@ return {
       },
     },
     -- Auto-format on save
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = "fallback",
-    },
+    -- format_on_save = {
+    --   timeout_ms = 500,
+    --   lsp_format = "fallback",
+    -- },
   },
 }
