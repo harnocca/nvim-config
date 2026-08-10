eab1212ac63526b9bd3afe200f4b1528f350dcb0
Harnocca <hauseowo@outlook.com>
2026-08-10 23:48:16 +0800

    refactor(lsp): separate lazydev configuration from lsp setup

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -4,16 +4,6 @@ return {
     dependencies = {
       "mason-org/mason.nvim",
       "mason-org/mason-lspconfig.nvim",
-      {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-          library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-            { path = "snacks.nvim", words = { "Snacks" } },
-          },
-        },
-      },
       { "mfussenegger/nvim-jdtls" },
     },
     config = function()
